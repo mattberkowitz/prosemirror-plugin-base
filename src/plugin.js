a22fcf2fd9c2d5fd20401f8cbb4595f0756bc387
mattberkowitz <berkowitz.matthew@gmail.com>
2016-02-11 14:00:37 -0500

    Update plugin.js

--- src/plugin.js
+++ src/plugin.js
@@ -1,8 +1,9 @@
 export class Plugin {
 	constructor(pm, options) {
 		this.pm = pm
+		
+		//setup default options and override with passed in options
 		this.options = this.defaultOptions
-
 		for(let option in options) this.options[option] = options[option]
 
 		//build up
